6a8cad75cda67f8867ac66c96bae8586b4009fdf
FantasticMachine <kotlik.vojtech@juniorlondynska.cz>
2026-07-18 20:37:25 +0200

    questions now load as elements successfully

--- app/js/question_fetch.js
+++ app/js/question_fetch.js
@@ -1,4 +1,5 @@
 let port = prompt("insert current port");
+const questionContainerElement = document.getElementById("question_display");
 
 // lets try fetch
 async function getQuestion() {
@@ -6,53 +7,38 @@ async function getQuestion() {
         let valid = true;
         let id = 1;
         while (valid) {
+            // change url
             let url = `http://localhost:${port}/question?id=${id}`;
+            // get json
             let response = await fetch(url);
-            if (response.ok) {
-                const data = await response.json();
-                console.log(url)
-                console.log(data);
-                id++;
-                localStorage.setItem(`question_${data.questionID}`,JSON.stringify(data));
-            } else {
-                valid = false;
-                throw new Error('Failed to fetch data');
+            // catch wrong
+            if (!response.ok) {
+                console.log(`[JS] Server returned status ${response.status}. Stopping loop.`);
+                break;
             }
+            // if correct add into local and as element
+            let question = await response.json();
+
+            // add to local memory
+            localStorage.setItem(`question_${id}`, JSON.stringify(question));
+
+            // create question element
+            let parent = document.createElement("div");
+            parent.classList.add("question");
+            // add heading
+            let heading = document.createElement("span");
+            heading.classList.add("question_heading");
+            heading.innerText = `(${question.questionID}) ${question.assignment}`;
+            // add to container
+            parent.appendChild(heading);
+            questionContainerElement.appendChild(parent);
+
+            // increase id
+            id++;
+
         }
     } catch (error) {
         console.error('Error:', error);
     }
 }
-getQuestion().then(() => {
-    // lets loop every question just as a test
-    // -- 1. get parent
-    let parentElement = document.getElementById("question_display");
-
-    // -- 2. loop
-    let id = 1;
-    while (localStorage.getItem(`question_${id}`)) {
-        // get json
-        let json = JSON.parse( localStorage.getItem(`question_${id}`) );
-        // create parent element
-        let questionElement = document.createElement("div");
-        // add heading
-        let heading = document.createElement("span");
-        heading.classList.add("question_heading");
-        heading.innerText = `(${json.questionID}) ${json.assignment}`;
-        questionElement.appendChild(heading);
-
-        // add answers
-        let listOfAnswers = document.createElement("ul");
-         // add every one
-        for (let i = 0; i < json.answers.length; i++) {
-            let item = document.createElement("li");
-            item.innerText = json.answers[i];
-            if (i === json.correctIndex) {
-                item.style.backgroundColor = "limegreen";
-            }
-            listOfAnswers.appendChild(item);
-        }
-
-        questionElement.appendChild(listOfAnswers);
-    }
-});
+getQuestion();
