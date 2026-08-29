ff0f11383164b30115059782ce4bf645e44a6abd
FantasticMachine <kotlik.vojtech@juniorlondynska.cz>
2026-08-29 19:06:10 +0200

    shit doesnt work

--- src/main/java/com/testgen/restapi/api/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.testgen.restapi.api.repo;
-
-import com.testgen.restapi.api.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
